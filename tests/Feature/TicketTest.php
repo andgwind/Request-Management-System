1cd1a2e71e8b78ad11f3c92fdcd01b2881a5ecfd
andgwind <Andrew0189@yandex.ru>
2023-09-10 20:47:22 +0300

    test Ticket store by user

---
+++ tests/Feature/TicketTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\TicketStatus;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TicketTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_store_ticket_by_user(): void
+    {
+        $ticketData = [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'status' => TicketStatus::ACTIVE->value,
+            'message' => 'This is a sample ticket message',
+            'comment' => 'This is a sample comment',
+        ];
+
+        $response = $this->postJson('/api/v1/tickets', $ticketData);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseCount('tickets', 1);
+    }
+}
